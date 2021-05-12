c101d9297c099d2fad7f2095f9a1c73828165123
Paramveer Singh <60150347+Marzipan78@users.noreply.github.com>
2021-05-12 13:04:26 +0200

    The Data Shredder

---
+++ data_shredder.py
@@ -0,0 +1,15 @@
+class DataShredder():
+
+    def __init__(self,name):
+        self.name = name 
+
+    def terrorize(self):
+        print("Good-day WEAKLINGS, it is I, self.name")
+
+class Asswhoop(DataShredder):
+    def slogan(self):
+        print(f"{self.name} , you donnies are my slaves!"  )
+
+class Immer_Asswhoop(DataShredder):
+    def slogan(self):
+        print(f"{self.name} ,you donnies are and will always be my slaves! Bring my pancakes"  )
